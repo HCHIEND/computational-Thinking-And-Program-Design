546e743a01856f07fd9cafc0ac1b4039d7e95cb7
HCHIEND <61811606+HCHIEND@users.noreply.github.com>
2020-04-23 05:42:11 +0800

    Add files via upload

---
+++ week7.py
@@ -0,0 +1,12 @@
+# 從維基百科找出要記錄的梨泰院相關資訊
+## 網址 https://zh.wikipedia.org/wiki/梨泰院Class
+### 相關資訊：影集名稱、釋出年份、平均時間...
+
+
+serie_title = '梨泰院'
+release_year ='2020'
+average_playing_time = '60 minutes'
+
+print (serie_title)
+print (release_year)
+print (average_playing_time)
